f1efc08b1a8868f582a3b99d4fa4775dae7b8849
Шматовский Сергей Александрович <Shmatovskiy.SA@tgc1.local>
2020-04-23 14:26:53 +0300

    Post change migration

--- database/migrations/2019_08_30_110642_create_posts_table.php
+++ database/migrations/2019_08_30_110642_create_posts_table.php
@@ -11,12 +11,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title')->unique();
+            $table->text('title')->unique();
             $table->text('body');
             $table->string('public');
             $table->integer('views')->default(0);
             $table->string('image');
-            $table->string('textPreview');
+            $table->text('textPreview');
             $table->string('imageAlt');
             $table->timestamps();
         });
